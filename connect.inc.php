6311dc7fe431bec60ca96ee7919cb0fffd9536fd
vinayb21 <vinay.badhan21@gmail.com>
2017-12-05 13:48:28 +0530

    Database setting changes

--- connect.inc.php
+++ connect.inc.php
@@ -2,10 +2,10 @@
 
 $conn_error='Could not connect.';
 
-$mysqli_host='mysql://bb7b6428344601:8626c690@us-cdbr-iron-east-05.cleardb.net/heroku_7adf718788690b0?reconnect=true';
-$mysqli_user='root';
-$mysqli_pass='';
-$mysqli_db='a_database';
+$mysqli_host='us-cdbr-iron-east-05.cleardb.net';
+$mysqli_user='bb7b6428344601';
+$mysqli_pass='8626c690';
+$mysqli_db='heroku_7adf718788690b0';
 
 $conn=mysqli_connect($mysqli_host, $mysqli_user, $mysqli_pass, $mysqli_db);
 if(!$conn || !(mysqli_select_db($conn,$mysqli_db)))
