509488c44e2aca59b607b9c0ca421995731bc61a
vinayb21 <vinay.badhan21@gmail.com>
2017-12-05 17:02:29 +0530

    Rolling back

--- connect.inc.php
+++ connect.inc.php
@@ -2,10 +2,11 @@
 
 $conn_error='Could not connect.';
 
-$mysql_host='us-cdbr-iron-east-05.cleardb.net';
+/*$mysql_host='us-cdbr-iron-east-05.cleardb.net';
 $mysql_user='bb7b6428344601';
 $mysql_pass='8626c690';
 $mysql_db='heroku_7adf718788690b0';
+*/
 
 $conn=mysql_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
 if(!$conn || !(mysql_select_db($conn,$mysql_db)))
