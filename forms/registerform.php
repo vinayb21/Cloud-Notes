dc50724deb5ac6a4e496c383d315f2b8d15bb8cf
vinayb21 <vinay.badhan21@gmail.com>
2017-12-06 16:55:17 +0530

    Database changes

--- forms/registerform.php
+++ forms/registerform.php
@@ -26,7 +26,7 @@ require 'connect.inc.php';
                 else{
                     /*$query = "INSERT INTO `users` '(' 'firstname', 'lastname', 'password', 'email', 'date', 'profession', 'location', 'education') VALUES ('','".mysqli_real_escape_string($conn,$firstname)."','".mysqli_real_escape_string($conn,$lastname)."','".mysqli_real_escape_string($conn,md5($password))."','".mysqli_real_escape_string($conn,$email)."','".mysqli_real_escape_string($conn,$date)."','".mysqli_real_escape_string($conn,'Student')."','".mysqli_real_escape_string($conn,'Indore')."','".mysqli_real_escape_string($conn,'Under-graduate')."','','','uploads/default.png')";*/
 
-                    $query = "INSERT INTO users (firstname, lastname, email) VALUES ('John', 'Doe', 'john@example.com')"";
+                    $query = "INSERT INTO users (firstname, lastname, email) VALUES ('John', 'Doe', 'john@example.com')";
 
                     if($query_run = mysqli_query($conn,$query)){
                         echo "You are registered Succesfully";
